5501725af1d1da6fe3931a76862913fdd035ea9a
dstyle0210 <dstyle0210@gmail.com>
2016-10-06 10:28:32 +0900

    레이아웃 페이지 완료.

--- src/js/mod.js
+++ src/js/mod.js
@@ -5,10 +5,12 @@ $(function(){
 var Mod = {
     // 핸드폰 리스트 서포트 : Modernizr 필수
     phoneList : function() {
-        if (!Modernizr.nthchild) {
-            $(".mod-phoneList").each(function () {
-                $(this).find(".mod-phoneItem:first-child,.mod-phoneItem:nth-child(6n+0)").addClass("no-nthchild-left");
-                $(this).find(".mod-phoneItem:nth-child(-n+5)").addClass("no-nthchild-top");
+        var modPhoneList = $(".mod-phoneList");
+        if (modPhoneList.length && !Modernizr.nthchild) {
+            modPhoneList.each(function () {
+                var me = $(this);
+                me.find(".mod-phoneItem:first-child,.mod-phoneItem:nth-child(6n+0)").addClass("no-nthchild-left");
+                me.find(".mod-phoneItem:nth-child(-n+5)").addClass("no-nthchild-top");
             });
         };
     }
