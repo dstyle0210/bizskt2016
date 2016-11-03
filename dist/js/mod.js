07c125a905ed2dca3b7bc83404d7209f91d373f4
dstyle0210 <dstyle0210@gmail.com>
2016-11-03 14:46:28 +0900

    1. 툴팁 삭제 현상처리 2 , 3 해피포인트 잔액계산 , 프로그래스 링크추가

--- dist/js/mod.js
+++ dist/js/mod.js
@@ -27,3 +27,66 @@ var Mod = {
         };
     }
 };
+
+/**
+ * 프로그래스에 링크 삽입하는 스크립트.
+ * @param linkArr : 링크경로들(배열)
+ * @param n : 탭의 갯수
+ * @example : stepAppendLink( ["http://a.com","http://b.com"] , 4);
+ */
+function stepAppendLink(linkArr,n){
+    var stepDiv = $("#stepDiv");
+    var wid = (100/n)+"%";
+    $(linkArr).each(function(idx,link){
+        var a = $("<a href='"+link+"'></a>").attr("style","display:inline-block;width:"+wid+";height:60px");
+        stepDiv.append(a);
+    });
+};
+
+/**
+ * LNB의 해피포인트 계산기
+ * @param wrapperId : "#아이디" 로 삽입.
+ * @method happyPointFn.calc() // 남은 해피포인트 계단 및 입력하기
+ * @method happyPointFn.getOriginPoint() // 초기에 입력된 해피포인트 (숫자형)
+ * @method happyPointFn.getOptionPoint() // 옵션에 적용된 해피포인트 (숫자형)
+ * @method happyPointFn.getModulusPoint() // 남은 해피포인트 (숫자형)
+ */
+var happyPointClass = function(wrapperId){
+    this.init = function(wrapperId){
+        var o = {};
+        o.div = $(wrapperId);
+        o.originDiv = o.div.find(".point i").eq(0);
+        o.resultDiv = o.div.find(".point i").eq(1).css({"font-size":"21px"});
+        o.originPoint = (o.originDiv.html().match(/[0-9]/gi).join(""))*1;
+        this.o = o;
+    };
+    this.getOriginPoint = function(){
+        return this.o.originPoint;
+    };
+    this.getOptionPoint = function(){
+        var o = this.o;
+        var optPoint = 0;
+        o.div.find(".mod-accItem").each(function(){
+            var sel = $(this).find("select").val();
+            var point = $(this).attr("point")*1;
+            optPoint += sel*point;
+        });
+        return optPoint;
+    };
+    this.getModulusPoint = function(){
+        return (this.o.originPoint - this.getOptionPoint());
+    };
+    this.calc = function(){
+        var o = this.o;
+        var modulusPoint = this.getModulusPoint();
+        if(modulusPoint<0){
+            alert("해피포인트가 부족합니다.");
+            //   return;
+        };
+        var pointStr = (modulusPoint+"").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        o.resultDiv.html(pointStr+" point");
+    };
+
+    this.init(wrapperId);
+    this.calc();
+};
